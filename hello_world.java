ae6ebee569adfdf932f9cd61444898123ce8cd16
kamehdi <kamehdi97@gmail.com>
2017-04-22 11:28:55 -0400

    Add files via upload

---
+++ hello_world.java
@@ -0,0 +1,7 @@
+/******* This is a hello world program created with the use of Java******/
+
+public class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
